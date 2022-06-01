179f9077fc1389c953a78e205b19d2e13eca0dd5
tkimx7 <55491619+tkimx7@users.noreply.github.com>
2022-06-01 03:16:05 -0400

    Create README.R

---
+++ README.R
@@ -0,0 +1,11 @@
+############################################################################################################
+###### --- README --- ######################################################################################
+############################################################################################################
+# 
+# --- This is a sample of a project that I did comparing General Linear Model (GLM) and regularization (Lasso 
+# --- and Ridge Regression). Though Lasso and Ridge Regression are both linear regression models that penalize 
+# --- explanatory variables, Lasso unlike Ridge Regression is a feature selection model that penalizes beta 
+# --- coefficients to zero, that is to say it can eliminate explanatory variables entirely. The GLM also 
+# --- involves cross validation that penalizes number of parameters over likelihood, i.e. the Akaike Information 
+# --- Criterion, but AIC isn't necessarily a criterion for feature selection. For this reason, Lasso can be a 
+# --- better fit than both Ridge Regression and GLM.
